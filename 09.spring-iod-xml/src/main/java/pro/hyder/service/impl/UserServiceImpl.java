66ff4625533413093e7e69fa977664e58249f204
hyder <yanghuan0613@gmail.com>
2019-12-01 02:56:21 +0800

    IOC基于XML

---
+++ 09.spring-iod-xml/src/main/java/pro/hyder/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package pro.hyder.service.impl;
+
+import pro.hyder.service.UserService;
+
+/**
+ * @author Hyder
+ * @version 1.0
+ * @date 2019/11/30 下午11:58
+ */
+public class UserServiceImpl implements UserService {
+	@Override
+	public void saveUser() {
+		System.out.println("演示 Spring IOC");
+	}
+}
